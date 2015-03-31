1e22c8c4e16403067b36da11075e21d9e1146e9c
Karen Tanigawa <tanigawa.karen@gmail.com>
2015-03-30 20:32:39 -1000

    testing data and diacriticals

--- public/js/app.js
+++ public/js/app.js
@@ -13,9 +13,79 @@ angular
     .state('card', {
       url : '/card',
       templateUrl : "views/card.html",
+      controller: "CardController"
     })
     .state('list_all', {
       url : '/list_all',
       templateUrl : "views/list_all.html",
     });
-  }]);
+  }])
+  .controller('CardController', ['$scope', function($scope){
+    // have cards
+    // choose one at random
+    // display cards
+
+    // display one card
+    $scope.card = {
+      meaning : "Fire",
+      options : [
+        {
+          title : "ahi", //uppercase renders or do i need css?
+          definition : "fire",
+          correct : true
+        },        
+        {
+          title : "ʻahi",
+          definition : "tuna fish",
+          correct : false
+        },
+        {
+          title : "pale ahi",
+          definition : "flame retardant",
+          correct : false
+        },
+      ]
+    },
+    {
+      meaning : "sacred",
+      options : [
+        {
+          title : "ihi", //uppercase renders or do i need css?
+          definition : "to tear off, remove",
+          correct : false
+        },        
+        {
+          title : "ʻihi",
+          definition : "sacred",
+          correct : true
+        },
+        {
+          title : "hō ʻihi",
+          definition : "flame retardant",
+          correct : false
+        },
+      ]
+    },
+    {
+      meaning : "to accept a challenge",
+      options : [
+        {
+          title : "ʻaʻa",
+          definition : "to accept a challenge",
+          correct : true
+        },        
+        {
+          title : "ʻaʻā",
+          definition : "to burn, blaze, glow",
+          correct : false
+        },
+        {
+          title : "aʻa",
+          definition : "small root, rootlet, vein, artery, nerve, tendon, muscle.",
+          correct : false
+        },
+      ]
+    },
+
+  }])
+  ;
